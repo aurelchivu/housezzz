cfc0cc0138b4bce0be3f06e2d291db7a84524c12
aurelchivu <aurelchivuutcb@gmail.com>
2022-04-30 13:47:07 +0300

    Added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "src/components/layout";
+import Layout from "src/components/Layout";
 
 export default function Home() {
-  return <Layout main={<div>Home</div>} />;
+  return <Layout main={<div>Homeeee</div>} />;
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { FunctionComponent, ReactNode } from "react";
 import Link from "next/link";
 
@@ -6,10 +7,36 @@ interface IProps {
 }
 
 const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => null;
   return (
     <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
-      <nav className="bg-gray-800 style={{height: '64px'}}">nav</nav>
-      {main}
+      <nav className="bg-gray-800" style={{ height: "64px" }}>
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="home house"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{main}</main>
     </div>
   );
 };
